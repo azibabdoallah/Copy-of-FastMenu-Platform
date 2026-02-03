cbdc50e52bfadf7a3fa5d4567ee448003affcf97
azibabdoallah <azibabdoallah@gmail.com>
2026-02-03 09:18:51 +0100

    Update supabase.ts

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -5,6 +5,6 @@ import { createClient } from '@supabase/supabase-js';
 // ------------------------------------------------------------------
 
 const supabaseUrl = 'https://apdgwwzyeanxxlotwmpl.supabase.co';
-const supabaseKey = 'sb_publishable_zfDzsqNyBUMv9oVvS71n5Q_lANnBSHe';
+const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFwZGd3d3p5ZWFueHhsb3R3bXBsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg2OTA4MjcsImV4cCI6MjA4NDI2NjgyN30.6B6ZmJrlkX8woc5JichK0FKbMaQDbQDODeoqQHhT_u8
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
